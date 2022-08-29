cb55943bea1c21f3b457e6bfe9a8de75ed074ad5
LucienMorey <lucienp@live.com.au>
2022-08-29 11:14:49 +1000

    Update fsw/src/services/trans_udp.c
    Co-authored-by: Nathanael <36506137+NathanaelGandhi@users.noreply.github.com>

--- fsw/src/services/trans_udp.c
+++ fsw/src/services/trans_udp.c
@@ -127,7 +127,7 @@ int32 IO_TransUdpConfigSocket(IO_TransUdpConfig_t *config, IO_TransUdp_t *udp)
     if (status != OS_SUCCESS)
     {
         CFE_EVS_SendEvent(IO_LIB_TRANS_UDP_EID, CFE_EVS_EventType_ERROR,
-                          "IO_TransUDP Error: Cant init Source Address buffer");
+                          "IO_TransUDP Error: Can't init Source Address buffer");
         return IO_TRANS_UDP_BAD_INPUT_ERROR;
     }
 
